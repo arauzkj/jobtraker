f24a99666324c343d44ace1267a5eeffa7f0606c
kjarj54 <kjarj54@gmail.com>
2026-07-21 14:49:37 -0600

    [FEAT: FINISH SET-UP BETTER AUTH CORRECCTIONS AND prisma update]
    -Update set-up of better-auth
    -Update prisma
    -Corection dashboard page

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,20 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
+import { username } from "better-auth/plugins";
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {
-        provider: "postgresql", 
-    }),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    username()
+  ]
+  /*
+  socialProviders:{
+
+  }*/
 });
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,1 +1,3 @@
-export class UserRepository {}
+export class UserRepository {
+    
+}
