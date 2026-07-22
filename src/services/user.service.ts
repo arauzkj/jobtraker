21149e2a8c7b2b7e57c6b9d10d4dc7dca647162d
kjarj54 <kjarj54@gmail.com>
2026-07-22 13:01:59 -0600

    [FEAT: USER API] - Implement GET endpoint for fetching all users and refactor user service and repository

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,1 +1,23 @@
-export class UserService {}
+import { UserRepository } from "@/repositories/user.repository";
+
+export class UserService {
+  static async getAllUsers() {
+    try {
+      const users = await UserRepository.findAll();
+      if (!users || users.length === 0) {
+        return {
+          success: true,
+          data: [],
+        };
+      }
+
+      return {
+        success: true,
+        data: users,
+      };
+
+    } catch (error) {
+      throw new Error("Ocurrió un error al procesar la lista de usuarios");
+    }
+  }
+}
